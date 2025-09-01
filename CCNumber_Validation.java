60fb4e2694bd058230ebee9b780c104e412f8f81
weimja01 <weimja01@yahoo.com>
2025-09-01 13:36:44 -0400

    set up

---
+++ CCNumber_Validation.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+
+public class CCNumber_Validation {
+
+public static boolean isValid(long number){
+    return true;
+}
+
+public static int sumOfDoubleEvenPlace(long number){
+    if(number % 2 == 0 ){
+        if(number % 1 == 0){
+        }
+    }
+    int sum = 0;
+    return sum;
+}
+
+public static int getDigit(int number){
+    int digit = 0;
+    return digit;
+}
+
+public static int sumOfOddPlace(long number){
+    int sum = 0;
+    return sum;
+}
+
+public static boolean prefixMatched(long number, int d){
+    return true;
+
+}
+
+public static int getSize(long d){
+    int size = 0;
+    return size;
+}
+
+public static long getPrefix(long number, int k){
+    long prefix = 0;
+    return prefix;
+}
+
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a credit card number as a long interger: ");
+        long number = input.nextLong();
+
+        System.out.println(number);
+
+        input.close();
+
+    }
+}
